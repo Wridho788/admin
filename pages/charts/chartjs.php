cd4678eda86f8e6fbabfe84ea47f17f903ecbd52
Lenovo <wridho246@gmail.com>
2020-12-08 23:25:25 +0700

    eror chart pie

--- pages/charts/chartjs.php
+++ pages/charts/chartjs.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include "conf/conn.php";
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -158,6 +163,17 @@
               </div>
             </div>
             <div class="box-body">
+            <?php 
+             $sql = "SELECT * FROM excel ORDER BY l1('Voice P2P') DESC";
+             $result = $koneksi->query($sql);
+
+             $arrayPie = array();
+              if ($result->num_rows > 0) {
+                  while($row = $result->fetch_assoc()) {
+                      $arrayPie[] =  "["."'".$row['Voice P2P']."'".",".$row['penggunaan']."]";
+                  }
+              }
+				    ?>
               <canvas id="pieChart" style="height:250px"></canvas>
             </div>
             <!-- /.box-body -->
@@ -529,73 +545,41 @@
     //- PIE CHART -
     //-------------
     // Get context with jQuery - using jQuery's .get() method.
-    var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-    var pieChart = new Chart(pieChartCanvas);
-    var PieData = [
-      {
-        value: 700,
-        color: "#f56954",
-        highlight: "#f56954",
-        label: "Chrome"
+    $(function(){
+  var chart = new Highcharts.Chart({
+      chart: {
+          renderTo: 'contohGrafikPie',
+         type: 'pie',
+          options3d: {
+              enabled: true,
+              alpha: 45,
+              beta: 0
+          }
       },
-      {
-        value: 500,
-        color: "#00a65a",
-        highlight: "#00a65a",
-        label: "IE"
+      title: {
+          text: 'Sample Penggunaan PHP Framework'
       },
-      {
-        value: 400,
-        color: "#f39c12",
-        highlight: "#f39c12",
-        label: "FireFox"
+      tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b><br>Penggunaan : <b>{point.y}</b>'
       },
-      {
-        value: 600,
-        color: "#00c0ef",
-        highlight: "#00c0ef",
-        label: "Safari"
+      plotOptions: {
+          pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              depth: 35,
+              dataLabels: {
+                  enabled: true,
+                  format: '{point.name}'
+              }
+          }
       },
-      {
-        value: 300,
-        color: "#3c8dbc",
-        highlight: "#3c8dbc",
-        label: "Opera"
-      },
-      {
-        value: 100,
-        color: "#d2d6de",
-        highlight: "#d2d6de",
-        label: "Navigator"
-      }
-    ];
-    var pieOptions = {
-      //Boolean - Whether we should show a stroke on each segment
-      segmentShowStroke: true,
-      //String - The colour of each segment stroke
-      segmentStrokeColor: "#fff",
-      //Number - The width of each segment stroke
-      segmentStrokeWidth: 2,
-      //Number - The percentage of the chart that we cut out of the middle
-      percentageInnerCutout: 50, // This is 0 for Pie charts
-      //Number - Amount of animation steps
-      animationSteps: 100,
-      //String - Animation easing effect
-      animationEasing: "easeOutBounce",
-      //Boolean - Whether we animate the rotation of the Doughnut
-      animateRotate: true,
-      //Boolean - Whether we animate scaling the Doughnut from the centre
-      animateScale: false,
-      //Boolean - whether to make the chart responsive to window resizing
-      responsive: true,
-      // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-      maintainAspectRatio: true,
-      //String - A legend template
-      legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-    };
-    //Create pie or douhnut chart
-    // You can switch between pie and douhnut using the method below.
-    pieChart.Doughnut(PieData, pieOptions);
+      series: [{
+          type: 'pie',
+          name: 'Penggunaan Framework',
+          data: [<?= join($arrayPie,",") ?>],
+      }]
+  });
+ });
 
     //-------------
     //- BAR CHART -
